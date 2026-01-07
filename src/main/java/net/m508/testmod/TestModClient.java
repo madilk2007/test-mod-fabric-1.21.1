37479e6bc5be7c0ebd80761ccb1fe2d9eece2d80
Adil Khan <m.k.adil2007@gmail.com>
2026-01-06 22:31:48 -0500

    Initial commit

---
+++ src/main/java/net/m508/testmod/TestModClient.java
@@ -0,0 +1,10 @@
+package net.m508.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
